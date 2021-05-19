06571d93622ab23f29596440c83d5c60e20d4496
Roy Surles <rsurles@nailor.com>
2021-05-19 12:54:33 -0500

    Add CustomMessageHandler to add X-Correlation-ID to HttpRequests.

---
+++ src/RecipeApp.BlazorWasmBootstrap/Features/Shared/MessageHandlers/CustomMessageHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+using RecipeApp.BlazorWasmBootstrap.Features.Shared.Services;
+
+namespace RecipeApp.BlazorWasmBootstrap.Features.Shared.MessageHandlers
+{
+    public class CustomMessageHandler : DelegatingHandler
+    {
+        protected readonly ISessionService _sessionService;
+
+        public CustomMessageHandler(ISessionService sessionService) =>
+            _sessionService = sessionService;
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.Headers.Remove("X-Correlation-ID");
+            request.Headers.Add("X-Correlation-ID", _sessionService.TraceId.ToString());
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
