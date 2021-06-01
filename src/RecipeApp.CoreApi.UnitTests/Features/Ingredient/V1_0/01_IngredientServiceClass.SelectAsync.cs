55c1b61d6bf4908ce9161f8ed2071e94ce72d7f5
Roy Surles <rsurles@nailor.com>
2021-06-01 15:09:46 -0500

    Implemented IngredientServiceTests.SelectAllForIntroductionIdAsync tests.

---
+++ src/RecipeApp.CoreApi.UnitTests/Features/Ingredient/V1_0/01_IngredientServiceClass.SelectAsync.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+using FluentAssertions;
+
+using Moq;
+
+using RecipeApp.Shared.Features.Ingredient;
+
+using Xunit;
+
+namespace RecipeApp.CoreApi.UnitTests.Features.Ingredient.V1_0
+{
+    public partial class IngredientServiceTests
+    {
+        [Theory(DisplayName = "IngredientServiceTests.Select")]
+        [InlineData("00000000-0000-0000-0000-000000000000", HttpStatusCode.BadRequest, new string[] { "Id is required." })]
+        [InlineData("eb95c593-69b2-4483-8fc3-4f74726a317e", HttpStatusCode.NotFound, new string[] { "No data found." })]
+        [InlineData("eb95c593-69b2-4483-8fc3-4f74726a317e", HttpStatusCode.OK, new string[] { })]
+        public async Task Select_Should_Return_Correct_StatusCode(Guid id
+            , HttpStatusCode expectedHttpStatusCode
+            , string[] expectedMessages)
+        {
+            // Arrange
+            var returnInstructionDto = Equals(expectedHttpStatusCode, HttpStatusCode.OK)
+                ? new IngredientDto()
+                : null;
+
+            _ingredientRepositoryMock
+                .Setup(x => x.SelectAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(returnInstructionDto);
+
+            // Act
+            var actualApiResult = await _ingredientService.SelectAsync(id).ConfigureAwait(false);
+
+            // Assert
+            actualApiResult.HttpStatusCode.Should().Be(expectedHttpStatusCode);
+            actualApiResult.Messages.Count.Should().Be(expectedMessages.Length);
+            foreach (var expectedMessage in expectedMessages)
+                actualApiResult.Messages.Should().Contain(m => Equals(m.Message, expectedMessage));
+        }
+    }
+}
---
+++ src/RecipeApp.CoreApi.UnitTests/Features/Ingredient/V1_0/02_IngredientServiceTests.SelectAllForIntroductionIdAsync.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+using FluentAssertions;
+
+using Moq;
+
+using RecipeApp.Shared.Features.Ingredient;
+
+using Xunit;
+
+namespace RecipeApp.CoreApi.UnitTests.Features.Ingredient.V1_0
+{
+    public partial class IngredientServiceTests
+    {
+        [Theory(DisplayName = "IngredientServiceTests.SelectAllForIntroductionIdAsync")]
+        [InlineData("00000000-0000-0000-0000-000000000000", HttpStatusCode.BadRequest, new string[] { "Introduction Id is required." })]
+        [InlineData("eb95c593-69b2-4483-8fc3-4f74726a317e", HttpStatusCode.NotFound, new string[] { "No data found." })]
+        [InlineData("eb95c593-69b2-4483-8fc3-4f74726a317e", HttpStatusCode.OK, new string[] { })]
+        public async Task SelectAllForIntroductionIdAsync_Should_Return_Correct_StatusCode(Guid id
+            , HttpStatusCode expectedHttpStatusCode
+            , string[] expectedMessages)
+        {
+            // Arrange
+            var returnInstructionDto = Equals(expectedHttpStatusCode, HttpStatusCode.OK)
+                ? new List<IngredientDto> { new IngredientDto() }
+                : new List<IngredientDto>();
+
+            _ingredientRepositoryMock
+                .Setup(x => x.SelectAllForIntroductionIdAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(returnInstructionDto);
+
+            // Act
+            var actualApiResult = await _ingredientService.SelectAllForIntroductionIdAsync(id).ConfigureAwait(false);
+
+            // Assert
+            actualApiResult.HttpStatusCode.Should().Be(expectedHttpStatusCode);
+            actualApiResult.Messages.Count.Should().Be(expectedMessages.Length);
+            foreach (var expectedMessage in expectedMessages)
+                actualApiResult.Messages.Should().Contain(m => Equals(m.Message, expectedMessage));
+        }
+    }
+}
