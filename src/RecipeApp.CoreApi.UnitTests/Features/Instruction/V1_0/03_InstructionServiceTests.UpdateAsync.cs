d4f54e32d20c42d7a6e6a1a3e1bf7bd2d366e4b3
Roy Surles <rsurles@nailor.com>
2021-06-01 13:54:03 -0500

    Implemented InstructionServiceTests.Update tests.

---
+++ src/RecipeApp.CoreApi.UnitTests/Features/Instruction/V1_0/03_InstructionServiceTests.UpdateAsync.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+using FluentAssertions;
+
+using Moq;
+
+using RecipeApp.Shared.Features.Instruction;
+
+using Xunit;
+
+namespace RecipeApp.CoreApi.UnitTests.Features.Instruction.V1_0
+{
+    public partial class InstructionServiceTests
+    {
+        [Theory(DisplayName = "InstructionServiceTests.Update")]
+        [InlineData("00000000-0000-0000-0000-000000000000", "eb95c593-69b2-4483-8fc3-4f74726a317e", null, HttpStatusCode.BadRequest, new string[] { "Introduction Id is required." })]
+        [InlineData("eb95c593-69b2-4483-8fc3-4f74726a317e", "eb95c593-69b2-4483-8fc3-4f74726a317e", null, HttpStatusCode.BadRequest, new string[] { "The Description field is required." })]
+        [InlineData("eb95c593-69b2-4483-8fc3-4f74726a317e", "eb95c593-69b2-4483-8fc3-4f74726a317e", "", HttpStatusCode.BadRequest, new string[] { "The Description field is required." })]
+        [InlineData("eb95c593-69b2-4483-8fc3-4f74726a317e", "eb95c593-69b2-4483-8fc3-4f74726a317e", "Description", HttpStatusCode.OK, new string[] { })]
+        public async Task Update_Should_Return_Correct_StatusCode(Guid introductionId
+            , Guid instructionId
+            , string description
+            , HttpStatusCode expectedHttpStatusCode
+            , string[] expectedMessages)
+        {
+            // Arrange
+            var instructionDto = new InstructionDto { Id = instructionId, IntroductionId = introductionId, Description = description };
+
+            _instructionRepositoryMock
+                .Setup(x => x.InsertAsync(It.IsAny<InstructionDto>(), It.IsAny<string>()))
+                .ReturnsAsync(instructionDto);
+
+            // Act
+            var actualApiResult = await _instructionService.UpdateAsync(instructionDto, null).ConfigureAwait(false);
+
+            // Assert
+            actualApiResult.HttpStatusCode.Should().Be(expectedHttpStatusCode);
+            actualApiResult.Messages.Count.Should().Be(expectedMessages.Length);
+            foreach (var expectedMessage in expectedMessages)
+                actualApiResult.Messages.Should().Contain(m => Equals(m.Message, expectedMessage));
+        }
+    }
+}
