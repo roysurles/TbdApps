6ac5b0dc5314c774476820d7e19ebf0a2e50275f
Roy Surles <rsurles@nailor.com>
2021-10-22 11:40:38 -0500

    Add CustomDelegatingHandler for Api.

---
+++ src/Tbd.WebApi.Shared/Handlers/CustomDelegatingHandler.cs
@@ -0,0 +1,43 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Tbd.WebApi.Shared.Handlers
+{
+    public class CustomDelegatingHandler : DelegatingHandler
+    {
+        protected readonly IHttpContextAccessor _httpContextAccessor;
+        protected readonly HttpContext _httpContext;
+        protected readonly ILogger<CustomDelegatingHandler> _logger;
+
+        public CustomDelegatingHandler(IHttpContextAccessor httpContextAccessor
+            , ILogger<CustomDelegatingHandler> logger)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _httpContext = _httpContextAccessor.HttpContext;
+            _logger = logger;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var accessToken = await _httpContext.GetTokenAsync("access_token");
+            if (string.IsNullOrWhiteSpace(accessToken).Equals(false))
+                request.Headers.Authorization = new AuthenticationHeaderValue("bearer", accessToken);
+
+            var traceIdentifier = _httpContext.TraceIdentifier;
+            var headers = _httpContext.Request.Headers;
+            if (headers.ContainsKey("X-Correlation-ID") && !string.IsNullOrWhiteSpace(headers["X-Correlation-ID"]))
+                traceIdentifier = headers["X-Correlation-ID"].ToString();
+
+            request.Headers.Remove("X-Correlation-ID");
+            request.Headers.Add("X-Correlation-ID", traceIdentifier);
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
