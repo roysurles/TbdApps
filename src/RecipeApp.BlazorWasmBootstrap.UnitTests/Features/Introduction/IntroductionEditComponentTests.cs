d97031e6fb2fbaeb43ddf33961f11c58685e4cc4
Roy Surles <surlesr@earthlink.net>
2021-11-18 20:27:25 -0600

    Initial work on bUnit tests.

---
+++ src/RecipeApp.BlazorWasmBootstrap.UnitTests/Features/Introduction/IntroductionEditComponentTests.cs
@@ -0,0 +1,25 @@
+﻿using Bunit;
+
+namespace RecipeApp.BlazorWasmBootstrap.UnitTests.Features.Introduction
+{
+    public class IntroductionEditComponentTests : TestContext
+    {
+        //[Fact]
+        //public void Test1()
+        //{
+        //    // Arrange
+        //    JSInterop.Mode = JSRuntimeMode.Loose;
+        //    var builder = WebAssemblyHostBuilder.CreateDefault();
+
+        //    Services.AddSingleton<IWebAssemblyHostEnvironment>(_ => builder.HostEnvironment);
+        //    Services.AddSingleton<ISessionViewModel, SessionViewModel>();
+
+        //    var cut = RenderComponent<IntroductionEditComponent>();
+
+        //    // Act
+
+        //    // Assert
+        //    var m = cut.Find("div");
+        //}
+    }
+}
