60ce6d6cbe68be990286212fa0716642f571900b
Roy Surles <surlesr@earthlink.net>
2022-01-27 09:26:28 -0600

    Add IngredientTableEditComponent Unit Test.

---
+++ src/RecipeApp.BlazorWasmBootstrap.UnitTests/Features/Ingredient/IngredientTableEditComponentTests.cs
@@ -0,0 +1,40 @@
+﻿using Bunit;
+
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+using RecipeApp.BlazorWasmBootstrap.Features.Ingredient;
+using RecipeApp.BlazorWasmBootstrap.UnitTests.Shared.Extensions;
+
+using Xunit;
+using Xunit.Abstractions;
+
+namespace RecipeApp.BlazorWasmBootstrap.UnitTests.Features.Ingredient
+{
+    public class IngredientTableEditComponentTests : TestContext
+    {
+        protected readonly ITestOutputHelper _output;
+
+        public IngredientTableEditComponentTests(ITestOutputHelper output) =>
+            _output = output;
+
+        [Fact]
+        public void Should_Render()
+        {
+            // Arrange
+            JSInterop.Mode = JSRuntimeMode.Loose;
+            Services.AddDefaultServices();
+
+            var mockIngredientApiClientV1_0 = new Mock<IIngredientApiClientV1_0>();
+            var mockLogger = new Mock<ILogger<IngredientViewModel>>();
+
+            // Act
+            var cut = RenderComponent<IngredientTableEditComponent>(parameters =>
+                parameters.Add(p => p.IngredientViewModel, new IngredientViewModel(mockIngredientApiClientV1_0.Object, mockLogger.Object)));
+
+            // Assert
+            var divHtml = cut.Find("div");
+        }
+    }
+}
