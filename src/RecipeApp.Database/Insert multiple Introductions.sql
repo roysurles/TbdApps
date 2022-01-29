dacfff1da64c62a81a2a2b9632db55cb82ccbfb1
Roy Surles <surlesr@earthlink.net>
2022-01-29 10:32:55 -0600

    Add sql script to insert multiple introduction records for testing.

---
+++ src/RecipeApp.Database/Insert multiple Introductions.sql
@@ -0,0 +1,29 @@
+﻿
+USE Recipe
+
+INSERT INTO Introduction (Id, Title, Comment)
+VALUES
+ (NEWID(), 'Title 1', 'Comment 1'),
+ (NEWID(), 'Title 2', 'Comment 2'),
+ (NEWID(), 'Title 3', 'Comment 3'),
+ (NEWID(), 'Title 4', 'Comment 4'),
+ (NEWID(), 'Title 5', 'Comment 5'),
+ (NEWID(), 'Title 6', 'Comment 6'),
+ (NEWID(), 'Title 7', 'Comment 7'), 
+ (NEWID(), 'Title 8', 'Comment 8'),
+ (NEWID(), 'Title 9', 'Comment 9'),
+ (NEWID(), 'Title 10', 'Comment 10'),
+ (NEWID(), 'Title 11', 'Comment 11'),
+ (NEWID(), 'Title 12', 'Comment 12'),
+ (NEWID(), 'Title 13', 'Comment 13'),
+ (NEWID(), 'Title 14', 'Comment 14'),
+ (NEWID(), 'Title 15', 'Comment 15'), 
+ (NEWID(), 'Title 16', 'Comment 16'),
+ (NEWID(), 'Title 17', 'Comment 17'),
+ (NEWID(), 'Title 18', 'Comment 18'),
+ (NEWID(), 'Title 19', 'Comment 19'),
+ (NEWID(), 'Title 20', 'Comment 20'),
+ (NEWID(), 'Title 21', 'Comment 21'),
+ (NEWID(), 'Title 22', 'Comment 22'),
+ (NEWID(), 'Title 23', 'Comment 23'), 
+ (NEWID(), 'Title 24', 'Comment 24')
