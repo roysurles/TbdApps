5cf3a8702df91d2951a2a2fcd73a6e9cef1d2fe0
Roy Surles <surlesr@earthlink.net>
2023-08-29 13:41:41 -0500

    Add apilog adhoc query

---
+++ src/RecipeApp.Database/AdHoc/Query ApiLog.sql
@@ -0,0 +1,17 @@
+﻿use recipe
+go
+ -- select * from [dbo].[Introduction]
+
+select * from ApiLog
+order by ActionDateTimeOffset desc
+
+/*
+
+truncate table ApiLog
+
+DBCC SHRINKDATABASE (Recipe, 10);
+GO
+
+*/
+
+
