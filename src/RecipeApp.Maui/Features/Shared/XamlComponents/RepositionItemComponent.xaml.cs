25bb9471b44396795ca62f25ac077cb4bac1a49c
Roy Surles <surlesr@live.com>
2023-09-21 15:46:59 -0500

    Finish out commands and properties for RepositionItem component

--- src/RecipeApp.Maui/Features/Shared/XamlComponents/RepositionItemComponent.xaml.cs
+++ src/RecipeApp.Maui/Features/Shared/XamlComponents/RepositionItemComponent.xaml.cs
@@ -15,18 +15,51 @@ public partial class RepositionItemComponent : ContentView
         set => SetValue(CommandParameterProperty, value);
     }
 
+
     public static readonly BindableProperty MoveFirstCommandProperty =
         BindableProperty.Create(nameof(MoveFirstCommand), typeof(ICommand), typeof(RepositionItemComponent));
     public ICommand MoveFirstCommand
     {
-        get => (ICommand)GetValue(MoveFirstCommandProperty);
-        set => SetValue(MoveFirstCommandProperty, value);
+        get => (ICommand)GetValue(MoveUpCommandProperty);
+        set => SetValue(MoveUpCommandProperty, value);
     }
 
-    private void MoveFirstImageButton_Clicked(object sender, EventArgs e)
+
+    public static readonly BindableProperty MoveUpCommandProperty =
+        BindableProperty.Create(nameof(MoveUpCommand), typeof(ICommand), typeof(RepositionItemComponent));
+    public ICommand MoveUpCommand
     {
-        MoveFirstCommand?.Execute(CommandParameter);
+        get => (ICommand)GetValue(MoveUpCommandProperty);
+        set => SetValue(MoveUpCommandProperty, value);
     }
 
-    //public object CommandParameter { get; set; }
+
+    public static readonly BindableProperty MoveDownCommandProperty =
+        BindableProperty.Create(nameof(MoveDownCommand), typeof(ICommand), typeof(RepositionItemComponent));
+    public ICommand MoveDownCommand
+    {
+        get => (ICommand)GetValue(MoveDownCommandProperty);
+        set => SetValue(MoveDownCommandProperty, value);
+    }
+
+
+    public static readonly BindableProperty MoveLastCommandProperty =
+        BindableProperty.Create(nameof(MoveLastCommand), typeof(ICommand), typeof(RepositionItemComponent));
+    public ICommand MoveLastCommand
+    {
+        get => (ICommand)GetValue(MoveLastCommandProperty);
+        set => SetValue(MoveLastCommandProperty, value);
+    }
+
+    private void MoveFirstImageButton_Clicked(object sender, EventArgs e) =>
+        MoveFirstCommand?.Execute(CommandParameter);
+
+    private void MovueUpImageButton_Clicked(object sender, EventArgs e) =>
+        MoveUpCommand?.Execute(CommandParameter);
+
+    private void MoveDownImageButton_Clicked(object sender, EventArgs e) =>
+        MoveDownCommand?.Execute(CommandParameter);
+
+    private void MoveLastImageButton_Clicked(object sender, EventArgs e) =>
+        MoveLastCommand?.Execute(CommandParameter);
 }
