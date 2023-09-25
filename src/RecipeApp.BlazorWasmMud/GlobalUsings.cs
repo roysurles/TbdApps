83b0f5ba7046fd277c743966f387bf4f6f141db5
Roy Surles <surlesr@live.com>
2023-09-25 14:14:40 -0500

    Initial work on details page

--- src/RecipeApp.BlazorWasmMud/GlobalUsings.cs
+++ src/RecipeApp.BlazorWasmMud/GlobalUsings.cs
@@ -1,6 +1,22 @@
 global using System;
+global using System.Net.Http;
+global using System.Text.Json;
 global using System.Threading.Tasks;
 
-global using Microsoft.Extensions.Logging;
+global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
 
-global using RecipeApp.Shared.Models;global using RecipeApp.Shared.Features.Introduction;
+global using MudBlazor.Services;
+
+global using RecipeApp.Shared.Features.Details;
+global using RecipeApp.Shared.Features.Ingredient;
+global using RecipeApp.Shared.Features.Instruction;
+global using RecipeApp.Shared.Features.Introduction;
+global using RecipeApp.Shared.Features.Session;
+global using RecipeApp.Shared.MessageHandlers;
+global using RecipeApp.Shared.Models;
+
+global using Refit;
+
+global using Tbd.Shared.ApiResult;
--- src/RecipeApp.BlazorWasmMud/Program.cs
+++ src/RecipeApp.BlazorWasmMud/Program.cs
@@ -1,64 +1,54 @@
-using System;
-using System.Diagnostics.CodeAnalysis;
-using System.Net.Http;
-using System.Text.Json;
-using System.Threading.Tasks;
+namespace RecipeApp.BlazorWasmMud;
 
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-
-using MudBlazor.Services;
-
-using RecipeApp.Shared.Features.Introduction;
-using RecipeApp.Shared.Features.Session;
-using RecipeApp.Shared.MessageHandlers;
-using RecipeApp.Shared.Models;
-
-using Refit;
-
-using Tbd.Shared.ApiResult;
-
-namespace RecipeApp.BlazorWasmMud
+public static class Program
 {
-    public static class Program
+    public static Task Main(string[] args)
     {
-        public static Task Main(string[] args)
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+
+        var defaultJsonDeSerializerOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+
+        builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+        builder.Services.AddMudServices(config =>
         {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+            config.SnackbarConfiguration.ShowCloseIcon = true;
+            config.SnackbarConfiguration.VisibleStateDuration = 3000;
+        });
 
-            var defaultJsonDeSerializerOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        var apiUrlsOptionsModel = new ApiUrlsOptionsModel();
+        builder.Configuration.GetSection("ApiUrls").Bind(apiUrlsOptionsModel);
+        builder.Services.AddSingleton(_ => apiUrlsOptionsModel);
 
-            builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        builder.Services.AddSingleton<ISessionViewModel, SessionViewModel>();
+        builder.Services.AddScoped<CustomMessageHandler>();
+        builder.Services.AddTransient(typeof(IApiResultModel<>), typeof(ApiResultModel<>));
 
-            builder.Services.AddMudServices(config =>
-            {
-                config.SnackbarConfiguration.ShowCloseIcon = true;
-                config.SnackbarConfiguration.VisibleStateDuration = 3000;
-            });
+        builder.Services.AddRefitClient<IIntroductionApiClientV1_0>()
+            .ConfigureHttpClient(c => c.BaseAddress = new Uri(apiUrlsOptionsModel.CoreApiUrl))
+            .AddHttpMessageHandler<CustomMessageHandler>();
 
-            var apiUrlsOptionsModel = new ApiUrlsOptionsModel();
-            builder.Configuration.GetSection("ApiUrls").Bind(apiUrlsOptionsModel);
-            builder.Services.AddSingleton(_ => apiUrlsOptionsModel);
+        builder.Services.AddHttpClient<IIntroductionApiClientNativeV1_0>(client =>
+            client.BaseAddress = new Uri(apiUrlsOptionsModel.CoreApiUrl))
+            .AddHttpMessageHandler<CustomMessageHandler>()
+            .AddTypedClient<IIntroductionApiClientNativeV1_0>(client => new IntroductionApiClientNativeV1_0(client, "/api/v1.0/Introduction", defaultJsonDeSerializerOptions: defaultJsonDeSerializerOptions));
 
-            builder.Services.AddSingleton<ISessionViewModel, SessionViewModel>();
-            builder.Services.AddScoped<CustomMessageHandler>();
-            builder.Services.AddTransient(typeof(IApiResultModel<>), typeof(ApiResultModel<>));
+        builder.Services.AddTransient<IIntroductionViewModel, IntroductionViewModel>();
+        builder.Services.AddSingleton<IIntroductionSearchViewModel, IntroductionSearchViewModel>();
 
-            builder.Services.AddRefitClient<IIntroductionApiClientV1_0>()
-                .ConfigureHttpClient(c => c.BaseAddress = new Uri(apiUrlsOptionsModel.CoreApiUrl))
-                .AddHttpMessageHandler<CustomMessageHandler>();
+        builder.Services.AddRefitClient<IIngredientApiClientV1_0>()
+            .ConfigureHttpClient(c => c.BaseAddress = new Uri(apiUrlsOptionsModel.CoreApiUrl))
+            .AddHttpMessageHandler<CustomMessageHandler>();
+        builder.Services.AddTransient<IIngredientViewModel, IngredientViewModel>();
 
-            builder.Services.AddHttpClient<IIntroductionApiClientNativeV1_0>(client =>
-                client.BaseAddress = new Uri(apiUrlsOptionsModel.CoreApiUrl))
-                .AddHttpMessageHandler<CustomMessageHandler>()
-                .AddTypedClient<IIntroductionApiClientNativeV1_0>(client => new IntroductionApiClientNativeV1_0(client, "/api/v1.0/Introduction", defaultJsonDeSerializerOptions: defaultJsonDeSerializerOptions));
+        builder.Services.AddRefitClient<IInstructionApiClientV1_0>()
+            .ConfigureHttpClient(c => c.BaseAddress = new Uri(apiUrlsOptionsModel.CoreApiUrl))
+            .AddHttpMessageHandler<CustomMessageHandler>();
+        builder.Services.AddTransient<IInstructionViewModel, InstructionViewModel>();
 
-            builder.Services.AddTransient<IIntroductionViewModel, IntroductionViewModel>();
-            builder.Services.AddSingleton<IIntroductionSearchViewModel, IntroductionSearchViewModel>();
+        builder.Services.AddTransient<IDetailsPageViewModel, DetailsPageViewModel>();
 
-            return builder.Build().RunAsync();
-        }
+        return builder.Build().RunAsync();
     }
 }
