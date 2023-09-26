932c1f23a232e9e76211649882329d3eacf34c45
Roy Surles <surlesr@live.com>
2023-09-26 13:12:34 -0500

    Initial layout of IngredientTableEditComponent

---
+++ src/RecipeApp.BlazorWasmMud/Features/Shared/Extensions/ApiResultMessageModelTypeEnumerationExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace RecipeApp.BlazorWasmMud.Features.Shared.Extensions;
+
+public static class ApiResultMessageModelTypeEnumerationExtensions
+{
+    public static MudBlazor.Severity ToSeverity(this ApiResultMessageModelTypeEnumeration apiResultMessageModelTypeEnumeration)
+    {
+        /*
+            Information
+            Warning
+            Error
+            UnhandledException
+            Hidden
+            Deprecated
+        */
+
+        var severity = MudBlazor.Severity.Info;  // TODO:  default; probably need a better mapping
+        if (apiResultMessageModelTypeEnumeration.Equals(ApiResultMessageModelTypeEnumeration.Information))
+            severity = MudBlazor.Severity.Info;
+        if (apiResultMessageModelTypeEnumeration.Equals(ApiResultMessageModelTypeEnumeration.Error))
+            severity = MudBlazor.Severity.Error;
+        if (apiResultMessageModelTypeEnumeration.Equals(ApiResultMessageModelTypeEnumeration.UnhandledException))
+            severity = MudBlazor.Severity.Error;
+        if (apiResultMessageModelTypeEnumeration.Equals(ApiResultMessageModelTypeEnumeration.Warning))
+            severity = MudBlazor.Severity.Warning;
+
+        return severity;
+    }
+}
