4a8747ea8e1177c5b50889801062f1fc9234c6d6
Roy Surles <surlesr@live.com>
2023-12-12 13:03:45 -0600

    Add model binding example with nested collection to mvc as example reference

---
+++ src/RecipeApp.Mvc/Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿namespace RecipeApp.Mvc.Controllers;
+public class StudentController : Controller
+{
+    public IActionResult Index()
+    {
+        var model = new StudentViewModel();
+        model.StudentAddress.Add(new StudentAddressViewModel());
+        model.StudentAddress.Add(new StudentAddressViewModel());
+
+        return View(model);
+    }
+
+    [HttpPost]
+    public IActionResult Index(StudentViewModel studentViewModel)
+    {
+        return View(studentViewModel);
+    }
+
+    [HttpPost]
+    public IActionResult Add(StudentViewModel studentViewModel)
+    {
+        studentViewModel.StudentAddress.Add(new StudentAddressViewModel());
+        return View("Index", studentViewModel);
+    }
+}
---
+++ src/RecipeApp.Mvc/Models/StudentViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace RecipeApp.Mvc.Models;
+
+public class StudentViewModel
+{
+    public StudentViewModel()
+    {
+        StudentAddress ??= new();
+    }
+
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    [BindRequired]
+    public List<StudentAddressViewModel> StudentAddress { get; set; }
+}
+
+public class StudentAddressViewModel
+{
+    public string? Address { get; set; }
+    public string? City { get; set; }
+}
