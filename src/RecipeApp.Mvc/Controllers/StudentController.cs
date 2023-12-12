a9b1e7832f335af6630e38b5847ff222af5c65e0
Roy Surles <surlesr@live.com>
2023-12-12 16:24:12 -0600

    remove un-needed code

--- src/RecipeApp.Mvc/Controllers/StudentController.cs
+++ src/RecipeApp.Mvc/Controllers/StudentController.cs
@@ -4,9 +4,6 @@ public class StudentController : Controller
     public IActionResult Index()
     {
         var model = new StudentViewModel();
-        model.StudentAddress.Add(new StudentAddressViewModel());
-        model.StudentAddress.Add(new StudentAddressViewModel());
-
         return View(model);
     }
 
@@ -20,8 +17,6 @@ public class StudentController : Controller
     public IActionResult Add(StudentViewModel studentViewModel)
     {
         studentViewModel.StudentAddress.Add(new StudentAddressViewModel());
-
-        ModelState.Clear();
         return View("Index", studentViewModel);
     }
 }
